f6cc28d6f44005115c720b37fc3fd9dcac3f44bf
Nicolás Fabián Mamani <nfmamani@suppliers.tenaris.com>
2023-11-07 21:44:11 -0300

    UI Home, UsualPayment and Achievement prototype

--- Darumi_Frontend/App/Screens/Home.js
+++ Darumi_Frontend/App/Screens/Home.js
@@ -1,18 +1,124 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, Modal, TextInput, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
 import { useAuth } from '@clerk/clerk-expo';
 import Header from '../Components/Home/Header';
 import MonthInfo from '../Components/Home/MonthInfo';
+import { Picker } from '@react-native-picker/picker';
 
 export default function Home() {
-    const { isLoaded,signOut } = useAuth();
+  
+  const { isLoaded, signOut } = useAuth();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [categoria, setCategoria] = useState('');
+  const [valor, setValor] = useState('');
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('');
+
+  const handleGuardarGasto = () => {
+    // Aquí puedes manejar la lógica para guardar el gasto
+    // Por ejemplo, enviar los datos a una API o almacenarlos localmente
+    console.log('Categoría:', categoriaSeleccionada);
+    console.log('Valor:', valor);
+    // Lógica adicional para guardar el gasto
+    // ...
+    // Cerrar el modal después de guardar el gasto
+    setCategoriaSeleccionada('');
+    setModalVisible(false);
+  };
+
   return (
-    <View style={{padding:20, 
-                  marginTop:25}}>
-      <Header/>
-        {/* <Button title='SignOut'
-                onPress={() =>signOut()}></Button> */}
-      <MonthInfo/>
+    <View style={{ padding: 20, marginTop: 25 }}>
+      <Header />
+      <MonthInfo />
+      <Button title="Agregar Gasto" onPress={() => setModalVisible(true)} />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalHeader}>Agregar Gasto</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Titulo"
+            keyboardType="text"
+            value={valor}
+            onChangeText={(text) => setValor(text)}
+            placeholderTextColor="#333" // Color del texto de placeholder
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Detalle"
+            keyboardType="text"
+            value={valor}
+            onChangeText={(text) => setValor(text)}
+            placeholderTextColor="#333" // Color del texto de placeholder
+          />
+          <Picker
+            selectedValue={categoriaSeleccionada}
+            onValueChange={(itemValue) => setCategoriaSeleccionada(itemValue)}
+            style={styles.picker}
+          >
+            <Picker.Item label="Seleccione una categoría" value="" />
+            <Picker.Item label="Comida" value="Comida" />
+            <Picker.Item label="Transporte" value="Transporte" />
+            <Picker.Item label="Entretenimiento" value="Entretenimiento" />
+            {/* Agrega más categorías según sea necesario */}
+          </Picker>
+          <TextInput
+            style={styles.input}
+            placeholder="Valor"
+            keyboardType="numeric"
+            value={valor}
+            onChangeText={(text) => setValor(text)}
+            placeholderTextColor="#333" // Color del texto de placeholder
+          />
+          <View style={styles.buttonsContainer}>
+            <Button title="Guardar" onPress={handleGuardarGasto} color="#007bff" />
+            <Button title="Cancelar" onPress={() => setModalVisible(false)} color="#dc3545" />
+          </View>
+        </View>
+      </Modal>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff', // Fondo blanco para el modal
+  },
+  modalHeader: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#007bff', // Color del encabezado
+  },
+  picker: {
+    height: 50,
+    width: '80%',
+    borderColor: '#007bff', // Color del borde del input
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    color: '#333', // Color del texto del input
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#007bff', // Color del borde del input
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    color: '#333', // Color del texto del input
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+});
--- Darumi_Frontend/App/Screens/UsualPayment.js
+++ Darumi_Frontend/App/Screens/UsualPayment.js
@@ -1,10 +1,129 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, TextInput, Button } from 'react-native';
 
 export default function UsualPayment() {
+  const [payments, setPayments] = useState([
+    { id: '1', name: 'Alquiler', amount: 1000 },
+    { id: '2', name: 'Servicios', amount: 200 },
+    // Agrega más pagos habituales según sea necesario
+  ]);
+
+  const [newPaymentName, setNewPaymentName] = useState('');
+  const [newPaymentAmount, setNewPaymentAmount] = useState('');
+
+  const totalAmount = payments.reduce((total, payment) => total + payment.amount, 0);
+
+  const handleAddPayment = () => {
+    if (newPaymentName && newPaymentAmount) {
+      const newPayment = {
+        id: Math.random().toString(),
+        name: newPaymentName,
+        amount: parseFloat(newPaymentAmount),
+      };
+      setPayments([...payments, newPayment]);
+      setNewPaymentName('');
+      setNewPaymentAmount('');
+    }
+  };
+
+  const handleRemovePayment = (id) => {
+    const updatedPayments = payments.filter((payment) => payment.id !== id);
+    setPayments(updatedPayments);
+  };
+
   return (
-    <View>
-      <Text>UsualPayment</Text>
+    <View style={styles.container}>
+      <Text style={styles.header}>Pagos Habituales</Text>
+      <FlatList
+        data={payments}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.paymentItem}>
+            <Text style={styles.paymentName}>{item.name}</Text>
+            <Text style={styles.paymentAmount}>${item.amount}</Text>
+            <TouchableOpacity onPress={() => handleRemovePayment(item.id)}>
+              <Text style={styles.deleteButton}>Eliminar</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+      <View style={styles.totalContainer}>
+        <Text style={styles.totalLabel}>Total:</Text>
+        <Text style={styles.totalAmount}>${totalAmount}</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre del pago"
+        value={newPaymentName}
+        onChangeText={(text) => setNewPaymentName(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Monto"
+        keyboardType="numeric"
+        value={newPaymentAmount}
+        onChangeText={(text) => setNewPaymentAmount(text)}
+      />
+      <Button title="Agregar Pago" onPress={handleAddPayment} />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f9f9f9',
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+  },
+  paymentItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: '#ffffff',
+    elevation: 2,
+  },
+  paymentName: {
+    fontSize: 16,
+    color: '#333',
+  },
+  paymentAmount: {
+    fontSize: 16,
+    color: '#007bff',
+  },
+  deleteButton: {
+    color: '#ff4500',
+    fontSize: 14,
+  },
+  totalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  totalLabel: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  totalAmount: {
+    fontSize: 18,
+    color: '#007bff',
+  },
+  input: {
+    height: 40,
+    borderColor: '#007bff',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    backgroundColor: '#ffffff',
+  },
+});
